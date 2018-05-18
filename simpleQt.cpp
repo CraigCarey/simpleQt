01bd2a1404745fa701ab09c0fa1a74a10aaed6d1
Craig <sgccarey@gmail.com>
2018-05-18 14:39:54 +0100

    Added CMakeLists.txt

--- simpleQt.cpp
+++ simpleQt.cpp
@@ -5,30 +5,37 @@
 
 // Build using qmake:
 // $ export QT_SELECT=5.10.1
-// $ export QT_QPA_PLATFORM_PLUGIN_PATH=/opt/qt/5.10.1/gcc_64/plugins
-// $ qmake -project           # creates .pro file
 // $ qmake                    # creates Makefile
 // $ make                     # builds target
+// $ export QT_QPA_PLATFORM_PLUGIN_PATH=/opt/qt/5.10.1/gcc_64/plugins
+// ./simpleQt
+
+// Build using cmake:
+// $ mkdir build && cd build
+// $ cmake                    # creates Makefile
+// $ make                     # builds target
+// $ export QT_QPA_PLATFORM_PLUGIN_PATH=/opt/qt/5.10.1/gcc_64/plugins
+// $ ./simpleQt
 
 #include <QApplication>
 #include <QPushButton>
 
 int main(int argc, char **argv)
 {
-    // QApplication handles the event loop for us
-    QApplication app (argc, argv);
+	// QApplication handles the event loop for us
+	QApplication app (argc, argv);
 
-    QPushButton button;
-    button.setText("My text");
-    button.setToolTip("A tooltip");
+	QPushButton button;
+	button.setText("My text");
+	button.setToolTip("A tooltip");
 
-    QFont font("courier");
-    button.setFont(font);
+	QFont font("courier");
+	button.setFont(font);
 
-    QIcon icon("/tmp/icon.png");
-    button.setIcon(icon);
+	QIcon icon("/tmp/icon.png");
+	button.setIcon(icon);
 
-    button.show();
+	button.show();
 
-    return app.exec();
+	return app.exec();
 }
